cc3ffcfc2c0d45bbe07064e473194d9fa9b48a02
brunoczim <brunoczim@gmail.com>
2019-12-20 20:55:03 -0300

    more keybindings and fixed it to work with other positions then the end

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
-var keybindings = {
-  '\\': '\\',
-  'a': 'ɑ',
-  'ae': 'æ',
-  'aa': 'ɐ',
-  'ao': 'ɒ',
-  'b': 'ɓ',
-  'bv': 'β',
-  'br': 'ʙ',
-  'c': 'ç',
-  'cs': 'ɕ',
-  'd': 'ɗ',
-  'dh': 'ð',
-  'dr': 'ɖ',
-  '~': '\u0303'
-};
+var letters = [
+  { input: '\\', output: '\\' },
+  { input: 'a', output: 'ɑ' },
+  { input: 'ae', output: 'æ' },
+  { input: 'aa', output: 'ɐ' },
+  { input: 'ao', output: 'ɒ' },
+  { input: 'b', output: 'ɓ' },
+  { input: 'bv', output: 'β' },
+  { input: 'B', output: 'ʙ' },
+  { input: 'c', output: 'ç' },
+  { input: 'cs', output: 'ɕ' },
+  { input: 'd', output: 'ɗ' },
+  { input: 'dh', output: 'ð' },
+  { input: 'dr', output: 'ɖ' },
+  { input: 'e', output: 'ə' },
+  { input: 'er', output: 'ɚ' },
+  { input: 'ee', output: 'ɘ' },
+  { input: 'e3', output: 'ɛ' },
+  { input: '3e', output: 'ɜ' },
+  { input: '3r', output: 'ɝ' },
+  { input: '3o', output: 'ɞ' },
+  { input: 'f', output: 'ɸ' },
+  { input: 'g', output: 'ɠ' },
+  { input: 'G', output: 'ɢ' },
+  { input: 'Gg', output: 'ʛ' },
+  { input: 'h', output: 'ɦ' },
+  { input: 'ha', output: 'ħ' },
+  { input: 'hs', output: 'ɧ' },
+  { input: 'H', output: 'ʜ' },
+];
 
-var diacritics = { '~': '' };
+var diacritics = [
+  { input: '~', output: '\u0303'}
+];
+
+var keyBindings = {};
+for (var i = 0; i < letters.length; i++) {
+  keyBindings[letters[i].input] = letters[i].output;
+}
+for (var i = 0; i < diacritics.length; i++) {
+  diacritics[letters[i].input] = diacritics[i].output;
+}
+
 
 renderKeyBindings();
 listenKeys();
@@ -23,14 +48,19 @@ listenKeys();
 function renderKeyBindings() {
   var elem = document.getElementById('keys-list');
 
-  for (keybinding in keybindings) {
+  for (var i = 0; i < letters.length; i++) {
     var item = document.createElement('li');
-    item.textContent = keybinding;
+    item.textContent = letters[i].input;
     item.textContent += ' → ';
-    if (keybinding in diacritics) {
-      item.textContent += '◌';
-    }
-    item.textContent += keybindings[keybinding];
+    item.textContent += letters[i].output;
+    elem.appendChild(item);
+  }
+
+  for (var i = 0; i < diacritics.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = diacritics[i].input;
+    item.textContent += ' → ◌';
+    item.textContent += diacritics[i].output;
     elem.appendChild(item);
   }
 }
@@ -50,9 +80,14 @@ function listenKeys() {
     switch (mode) {
       case 'special':
         if (event.key == 'Enter') {
-          var buf = elem.value.substring(specialPos + 1, elem.value.length - 1);
-          if (buf in keybindings) {
-            elem.value = elem.value.substring(0, specialPos) + keybindings[buf];
+          var end = elem.selectionStart;
+          var buf = elem.value.substring(specialPos + 1, end - 1);
+          if (buf in keyBindings) {
+            var prefix = elem.value.substring(0, specialPos);
+            var suffix = elem.value.substring(end);
+            elem.value = prefix + keyBindings[buf] + suffix;
+            elem.selectionStart = specialPos + 1;
+            elem.selectionEnd = specialPos + 1;
           }
           setMode('normal');
         }  else if (event.key == 'Escape') {
@@ -63,7 +98,7 @@ function listenKeys() {
         if (event.key == '\\') {
           setMode('special');
           tempBuf = '';
-          specialPos = elem.value.length - 1;
+          specialPos = elem.selectionStart - 1;
         }
         break;
     }
