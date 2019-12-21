af9c6f581a75579822dfdc9b81847bb4347e49ae
brunoczim <brunoczim@gmail.com>
2019-12-20 21:27:39 -0300

    complementing functionality on current symbol

--- js/main.js
+++ js/main.js
@@ -1,46 +1,74 @@
 var letters = [
-  { input: '\\', output: '\\' },
-  { input: 'a', output: 'ɑ' },
-  { input: 'ae', output: 'æ' },
-  { input: 'aa', output: 'ɐ' },
-  { input: 'ao', output: 'ɒ' },
-  { input: 'b', output: 'ɓ' },
-  { input: 'bv', output: 'β' },
-  { input: 'B', output: 'ʙ' },
-  { input: 'c', output: 'ç' },
-  { input: 'cs', output: 'ɕ' },
-  { input: 'd', output: 'ɗ' },
-  { input: 'dh', output: 'ð' },
-  { input: 'dr', output: 'ɖ' },
-  { input: 'e', output: 'ə' },
-  { input: 'er', output: 'ɚ' },
-  { input: 'ee', output: 'ɘ' },
-  { input: 'e3', output: 'ɛ' },
-  { input: '3e', output: 'ɜ' },
-  { input: '3r', output: 'ɝ' },
-  { input: '3o', output: 'ɞ' },
-  { input: 'f', output: 'ɸ' },
-  { input: 'g', output: 'ɠ' },
-  { input: 'G', output: 'ɢ' },
-  { input: 'Gg', output: 'ʛ' },
-  { input: 'h', output: 'ɦ' },
-  { input: 'ha', output: 'ħ' },
-  { input: 'hs', output: 'ɧ' },
-  { input: 'H', output: 'ʜ' },
+  { input: '\\', output: '\\', name: 'backslash' },
+  { input: 'a', output: 'ɑ', name: 'open back unrounded vowel' },
+  { input: 'ae', output: 'æ', name: 'near-open front unrounded vowel' },
+  { input: 'aa', output: 'ɐ', name: 'near-open central unrounded vowel' },
+  { input: 'ao', output: 'ɒ', name: 'open back rounded vowel' },
+  { input: 'b', output: 'ɓ', name: 'voiced bilabial implosive' },
+  { input: 'bv', output: 'β', name: 'voiced bilabial fricative' },
+  { input: 'B', output: 'ʙ', name: 'voiced bilabial trill' },
+  { input: 'c', output: 'ç', name: 'voiceless palatal fricative' },
+  { input: 'cs', output: 'ɕ', name: 'voiceless alveolo-palatal fricative' },
+  { input: 'd', output: 'ɗ', name: 'voiced alveolar implosive' },
+  { input: 'dh', output: 'ð', name: 'voiced dental fricative' },
+  { input: 'dr', output: 'ɖ', name: 'voiced retroflex stop' },
+  { input: 'e', output: 'ə', name: 'mid central vowel' },
+  { input: 'er', output: 'ɚ', name: 'rhotacized mid central vowel' },
+  { input: 'ee', output: 'ɘ', name: 'close-mid central unrounded vowel' },
+  { input: 'e3', output: 'ɛ', name: 'open-mid front unrounded vowel' },
+  { input: '3e', output: 'ɜ', name: 'open-mid central unrounded vowel' },
+  { input: '3r', output: 'ɝ', name: 'rhotacized open-mid central unrounded vowel' },
+  { input: '3o', output: 'ɞ', name: 'open-mid central rounded vowel' },
+  { input: 'f', output: 'ɸ', name: 'voiceless bilabial fricative' },
+  { input: 'g', output: 'ɠ', name: 'voiced velar implosive' },
+  { input: 'G', output: 'ɢ', name: 'voiced uvular stop' },
+  { input: 'Gg', output: 'ʛ', name: 'voiced uvular implosive' },
+  { input: 'h', output: 'ɦ', name: 'breathy-voiced glottal transition' },
+  { input: 'ha', output: 'ħ', name: 'voiceless pharyngeal fricative' },
+  { input: 'hs', output: 'ɧ', name: 'voiceless palatal-velar fricative' },
+  { input: 'H', output: 'ʜ', name: 'voiceless epiglottal fricative' },
 ];
 
 var diacritics = [
-  { input: '~', output: '\u0303'}
+  { input: '~', output: '\u0303', name: 'nasalized' }
 ];
+
+var nonKeyPhones = {
+  'a': 'open front unrounded vowel',
+  'b': 'voiced bilabial stop',
+  'c': 'voiceless palatal stop',
+  'd': 'voiced alveolar stop',
+  'e': 'close-mid front unrounded vowel',
+  'f': 'voiceless labiodental fricative',
+  'g': 'voiced velar stop',
+  'h': 'voiceless glottal transition',
+  'i': 'close front unrounded vowel',
+  'j': 'palatal approximant',
+  'k': 'voiceless velar stop',
+  'l': 'alveolar lateral approximant',
+  'm': 'bilabial nasal',
+  'n': 'alveolar nasal',
+  'o': 'close-mid back rounded vowel',
+  'p': 'voiceless bilabial stop',
+  'q': 'voiceless uvular stop',
+  'r': 'alveolar trill',
+  's': 'voiceless alveolar fricative',
+  't': 'voiceless alveolar stop',
+  'u': 'close back rounded vowel',
+  'v': 'voiced labiodental fricative',
+  'w': 'voiced labio-velar approximant',
+  'x': 'voiceless velar fricative',
+  'y': 'close front rounded vowel',
+  'z': 'voiced alveolar fricative',
+};
 
 var keyBindings = {};
 for (var i = 0; i < letters.length; i++) {
-  keyBindings[letters[i].input] = letters[i].output;
+  keyBindings[letters[i].input] = letters[i];
 }
 for (var i = 0; i < diacritics.length; i++) {
-  diacritics[letters[i].input] = diacritics[i].output;
+  diacritics[letters[i].input] = diacritics[i];
 }
-
 
 renderKeyBindings();
 listenKeys();
@@ -67,6 +95,7 @@ function renderKeyBindings() {
 
 function listenKeys() {
   var elem = document.getElementById('typing-area');
+  var current = document.getElementById('current-symbol-text');
 
   var mode = 'normal';
   var specialPos = 0;
@@ -85,9 +114,10 @@ function listenKeys() {
           if (buf in keyBindings) {
             var prefix = elem.value.substring(0, specialPos);
             var suffix = elem.value.substring(end);
-            elem.value = prefix + keyBindings[buf] + suffix;
+            elem.value = prefix + keyBindings[buf].output + suffix;
             elem.selectionStart = specialPos + 1;
             elem.selectionEnd = specialPos + 1;
+            current.textContent = keyBindings[buf].name;
           }
           setMode('normal');
         }  else if (event.key == 'Escape') {
@@ -99,6 +129,8 @@ function listenKeys() {
           setMode('special');
           tempBuf = '';
           specialPos = elem.selectionStart - 1;
+        } else if (event.key in nonKeyPhones) {
+          current.textContent = nonKeyPhones[event.key];
         }
         break;
     }
